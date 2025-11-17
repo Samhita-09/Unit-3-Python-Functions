728741a3552e2dd97a2ee8ba710f152da9969f8b
Samhita <samkal28@bergen.org>
2025-11-17 17:37:42 -0500

    homework completed 11/12/2025

---
+++ Lesson 03 - Parameters/Practice/homework.py
@@ -0,0 +1,40 @@
+# Question 5
+
+# Output1: 18.00
+# Output2: 15.00
+
+# Question 6
+
+def make_notification(user, *messages, urgent=False):
+    if urgent:
+        urgent = "URGENT"
+    elif urgent == False:
+        urgent = "Not Urgent"
+    return f"{urgent}: {user} - {messages}"
+
+print(make_notification("admin", "Server down!", urgent=True))
+print(make_notification("user", "Welcome", "Check inbox"))
+
+# Question 7
+
+# Output1: SELECT name, email FROM users LIMIT 10
+# Output2: SELECT * FROM logs WHERE level='error' LIMIT 5
+
+# Question 8
+
+def log_action(actor, *actions, timestamp=None, **context):
+    actionsStr = ""
+    for action in actions:
+        if actionsStr == "":
+            actionsStr = action
+        elif action not in actionsStr:
+            actionsStr += ", " + action
+    contextStr = ""
+    for key, value in context.items():
+        if contextStr == "":
+            contextStr = f"{key}={value}"
+        elif key not in contextStr:
+            contextStr += ", " + f"{key}={value}"
+    return f"{actor}: {actionsStr} | {contextStr}"
+
+print(log_action("bot", "login", "scan", source="API", ip="1.2.3.4"))
